84b809d07551ba0b9828ba3e34877d54754f4ac4
Nick <NickyDaB@gmail.com>
2024-10-08 15:27:44 -0400

    added subtract method - Nick completed partner B work.

--- homework-2-methods-and-peer-work/homework-2-methods-and-peer-work/Program.cs
+++ homework-2-methods-and-peer-work/homework-2-methods-and-peer-work/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Partner A: Yuri
+            //Partner B: Nick
+
+            //Yuri should fix all the comments later :P check the rubric
+            int result;
+            int nickFavoriteNumber;
+            
+
+
+            //Initialize
+            result = 0;
+            nickFavoriteNumber = 37;
+
+
+            //Partner B function
+            int Subtract(int num1, int num2)
+            {
+                int answer = 0;
+                answer = num1 - num2;
+
+                return answer;
+            }
+
+            //Start of the actual program
+            Console.WriteLine("Nick's favorite number is : " + nickFavoriteNumber);
+
+            //Console.WriteLine("About to call Partner B's subtract method.");
+            //Console.WriteLine("When we subtract " + yuriFavoriteNumber + " from " + nickFavoriteNumber + " we get : " + result);
+            //Console.WriteLine("When we subtract " + 3 + " from " + nickFavoriteNumber + " we get : " + result);
+
+            result = Subtract(3, nickFavoriteNumber);
+
+            Console.WriteLine("When we subtract our favorite numbers from each other we get: " + result);
+
         }
     }
 }
