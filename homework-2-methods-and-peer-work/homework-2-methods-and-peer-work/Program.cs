3adc041bc459774bb5cabd4407f183f5bc33fd53
Nick <NickyDaB@gmail.com>
2024-10-08 15:13:00 -0400

    created VS project

---
+++ homework-2-methods-and-peer-work/homework-2-methods-and-peer-work/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace homework_2_methods_and_peer_work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
