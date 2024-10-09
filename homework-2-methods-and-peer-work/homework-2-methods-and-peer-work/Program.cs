babe15ab640a0f53ffd003544ea35597fcd2be8f
Snoe <yurikorolev99@gmail.com>
2024-10-09 13:38:12 -0400

    added static Add function

--- homework-2-methods-and-peer-work/homework-2-methods-and-peer-work/Program.cs
+++ homework-2-methods-and-peer-work/homework-2-methods-and-peer-work/Program.cs
@@ -2,22 +2,33 @@
 {
     internal class Program
     {
+
+        //Partner A Function
+        static int Add(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;   
+        }
         static void Main(string[] args)
         {
             //Partner A: Yuri
             //Partner B: Nick
 
             //Yuri should fix all the comments later :P check the rubric
+            //Variables
+            int sum;
             int result;
             int nickFavoriteNumber;
             int yuriFavoriteNumber;
-            
+
 
 
             //Initialize
+            sum = 0;
             result = 0;
             nickFavoriteNumber = 37;
             yuriFavoriteNumber = 16;
+
 
 
             //Partner B function
@@ -31,15 +42,20 @@
 
             //Start of the actual program
             Console.WriteLine("Nick's favorite number is : " + nickFavoriteNumber);
+            Console.WriteLine("Yuri's favorite number is : " + yuriFavoriteNumber);
 
             //Console.WriteLine("About to call Partner B's subtract method.");
             //Console.WriteLine("When we subtract " + yuriFavoriteNumber + " from " + nickFavoriteNumber + " we get : " + result);
             //Console.WriteLine("When we subtract " + 3 + " from " + nickFavoriteNumber + " we get : " + result);
 
-            result = Subtract(3, nickFavoriteNumber);
+            sum = Add(nickFavoriteNumber, yuriFavoriteNumber);
+            result = Subtract(yuriFavoriteNumber, nickFavoriteNumber);
 
+            Console.WriteLine("When we add our favorite numberes together we get: " + sum);
             Console.WriteLine("When we subtract our favorite numbers from each other we get: " + result);
+            
 
         }
+
     }
 }
